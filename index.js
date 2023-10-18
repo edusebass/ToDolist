eb0c867e3bda71ce2e63a3cb93de5a3fcba738b0
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-18 15:06:33 -0500

    project listo to do finished

--- index.js
+++ index.js
@@ -10,18 +10,14 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
+let items = [];
+let itemsWork = [];
+
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-
-
-app.get("/works", (req, res) => {
-    res.render("works");
-});
-
-let items = [];
 
 app.get("/", (req, res) => {
     res.render("index", { items: items });
@@ -33,7 +29,14 @@ app.post("/", (req, res) => {
     res.redirect("/"); 
 });
 
-
+app.get("/works", (req, res) => {
+    res.render("works", {itemsWork: itemsWork});
+});
+app.post("/works", (req,res) => {
+    const newWork = req.body.newItem;
+    itemsWork.push(newWork);
+    res.redirect("/works");
+});
 
 app.listen(port, () => {
     console.log(`listening on ${port}`);
