1bbd22d26b885974fa5baf2104f9c23b66ea7e13
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-18 10:51:58 -0500

    form css add

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+
 app.listen(port, () => {
     console.log(`listening on ${port}`);
 });
