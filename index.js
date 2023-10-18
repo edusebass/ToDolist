6c2ddc738ee21472111f0513d408929a0ffe6fa7
Eduardo Almachi <eduardoalmachi123@hotmail.com>
2023-10-18 10:32:57 -0500

    project init

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.listen(port, () => {
+    console.log(`listening on ${port}`);
+});
